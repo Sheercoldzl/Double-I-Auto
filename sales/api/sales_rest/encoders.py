bc99df00e50723a32dbb003ae8a3961c24a48350
Ian Li <ziyin@utexas.edu>
2023-01-27 20:32:43 +0000

    Update encoders.py

--- sales/api/sales_rest/encoders.py
+++ sales/api/sales_rest/encoders.py
@@ -1,17 +1,21 @@
 from common.json import ModelEncoder
 from .models import AutomobileVO, SalesPerson, PotentialCustomer, SalesRecord
 
+
 class AutomobileVOEncoder(ModelEncoder):
     model = AutomobileVO
     properties = ["vin"]
+
 
 class SalesPersonEncoder(ModelEncoder):
     model = SalesPerson
     properties = ["name", "employee_id"]
 
+
 class PotentialCustomerEncoder(ModelEncoder):
     model = PotentialCustomer
     properties = ["name", "address", "phone_number"]
+
 
 class SalesRecordEncoder(ModelEncoder):
     model = SalesRecord
